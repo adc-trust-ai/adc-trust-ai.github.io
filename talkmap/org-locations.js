ae3750bdb5a09a204a01c807143f761af4d48585
Albert Dorador Chalar <trustalgorithm.dev@gmail.com>
2025-10-24 12:04:15 +0200

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -46,7 +46,7 @@ var addressPoints = [
   ],
   [
     "Workshop on my TRUST algorithm (2025) <br />University of Seville; Seville, Spain",
-    37.388878,
-    -5.994606
+    37.20,
+    -5.58
   ]
 ];
